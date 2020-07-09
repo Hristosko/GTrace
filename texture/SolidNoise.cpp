753c9483654e37475f9e495c2863bec98362fcaa
Hristosko Chaushev <tosko_chauschev@abv.bg>
2020-07-09 23:42:19 +0300

    perlin noise impl

--- texture/SolidNoise.cpp
+++ texture/SolidNoise.cpp
@@ -4,16 +4,57 @@
 
 #include "../Random.h"
 
+const Vector3f SolidNoise::grad[SOLID_NOISE_SIZE] = {
+	Vector3f(1,1,0),
+	Vector3f(-1,1,0),
+	Vector3f(1,-1,0),
+	Vector3f(-1,-1,0),
+
+	Vector3f(1,0,1),
+	Vector3f(-1,0,1),
+	Vector3f(1,0,-1),
+	Vector3f(-1,0,-1),
+
+	Vector3f(0,1,1),
+	Vector3f(0,-1,1),
+	Vector3f(0,1,-1),
+	Vector3f(0,-1,-1),
+
+	Vector3f(1,1,0),
+	Vector3f(-1,1,0),
+	Vector3f(0,-1,1),
+	Vector3f(0,-1,-1),
+};
+
 SolidNoise::SolidNoise() {
-	for (int8_t i = 0; i < 16; ++i) {
+	for (int8_t i = 0; i < SOLID_NOISE_SIZE; ++i) {
 		this->phi[i] = i;
 	}
 
 	RandomGenerator rng;
-	for (int8_t i = 14; i >= 0; --i) {
+	for (int8_t i = SOLID_NOISE_SIZE - 2; i >= 0; --i) {
 		int8_t target = int8_t(rng.get() * i);
 		std::swap(this->phi[i + 1], this->phi[target]);
 	}
+}
+
+float SolidNoise::perlinNoise(const Vector3f& p) {
+	const int fi = static_cast<int>(floor(p.x()));
+	const int fj = static_cast<int>(floor(p.y()));
+	const int fk = static_cast<int>(floor(p.z()));
+	const Vector3f u = p - Vector3f((float)fi, (float)fj, (float)fk);
+	double sum = 0.0;
+
+	sum += knot(fi, fj, fk, u);
+	sum += knot(fi + 1, fj, fk, u - Vector3f(1.f, 0.f, 0.f));
+	sum += knot(fi, fj + 1, fk, u - Vector3f(0.f, 1.f, 0.f));
+	sum += knot(fi, fj, fk + 1, u - Vector3f(0.f, 0.f, 1.f));
+	sum += knot(fi + 1, fj + 1, fk, u - Vector3f(1.f, 1.f, 0.f));
+	sum += knot(fi + 1, fj, fk + 1, u - Vector3f(1.f, 0.f, 1.f));
+	sum += knot(fi, fj + 1, fk + 1, u - Vector3f(0.f, 1.f, 1.f));
+	sum += knot(fi + 1, fj + 1, fk + 1, u - Vector3f(1.f, 1.f, 1.f));
+
+	return static_cast<float>(sum);
 }
 
 // cubic wightning function
@@ -24,4 +65,16 @@ float SolidNoise::omega(float t) const {
 	const float t4 = t2 * t2;
 	const float t6 = t4 * t2;
 	return -6.f * t6 + 15.f * t4 - 10.f * t * t2 + 1;
+}
+
+Vector3f SolidNoise::gamma(int i, int j, int k) const {
+	int idx = phi[abs(k) % SOLID_NOISE_SIZE];
+	idx = phi[abs(j + idx) % SOLID_NOISE_SIZE];
+	idx = phi[abs(i + idx) % SOLID_NOISE_SIZE];
+	return grad[idx];
+}
+
+float SolidNoise::knot(int i, int j, int k, const Vector3f& v) const {
+	return omega(v.x())* omega(v.y())* omega(v.z())*
+		dot(gamma(i, j, k), v);
 }
--- texture/SolidNoise.h
+++ texture/SolidNoise.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #include <cstdint>
+#include "../math/Vector3f.h"
+
+#define SOLID_NOISE_SIZE 16
 
 class SolidNoise {
 public:
 	SolidNoise();
+
+	float perlinNoise(const Vector3f& p);
 private:
 	float omega(float t) const;
-	//float 
+	Vector3f gamma(int i, int j, int k) const;
+	float knot(int i, int j, int k, const Vector3f& v) const;
 private:
-	int8_t phi[16];
+	int8_t phi[SOLID_NOISE_SIZE];
+	static const Vector3f grad[SOLID_NOISE_SIZE];
 };
