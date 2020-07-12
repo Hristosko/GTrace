25d2d0d511666143d8982c6efc464c712ace00f0
Hristosko Chaushev <tosko_chauschev@abv.bg>
2020-07-12 17:48:22 +0300

    bvh initial impl

---
+++ geometry/BVH.cpp
@@ -0,0 +1,60 @@
+#include "BVH.h"
+#include "../Logger.h"
+
+///
+/// Quicksort-style split function to split the shapes according to the pivot value and axis
+///
+static uint32_t split(Shape** shapes, uint32_t cnt, float pivot, int axis) {
+	uint32_t lesser = 0;
+	for (uint32_t i = 0; i < cnt; ++i) {
+		const BBox bb = shapes[i]->bbox();
+		const float center = (bb.max() + bb.min())[axis] * 0.5f;
+		if (center < pivot) {
+			std::swap(shapes[i], shapes[lesser++]);
+		}
+	}
+	if (lesser == 0 || lesser == cnt) lesser = (cnt / 2);
+	return lesser;
+}
+
+static Shape* build(Shape** shapes, uint32_t cnt, int axis) {
+	if (cnt == 1) return shapes[0];
+	if (cnt == 2) return new BVH(shapes[0], shapes[1]);
+
+	// find the midpoint of the bounding box according the given axis
+	BBox box = shapes[0]->bbox();
+	for (uint32_t i = 0; i < cnt; ++i) {
+		box = BBox::bound(box, shapes[i]->bbox());
+	}
+	const Vector3f pivot = (box.max() - box.min()) * 0.5f;
+	uint32_t middle = split(shapes, cnt, pivot[axis], axis);
+
+	return new BVH(
+		build(shapes, middle, (axis + 1) % 3),
+		build(&shapes[middle], cnt - middle, (axis + 1) % 3),
+		box);
+}
+
+Shape* BVH::build(Shape** shapes, uint32_t cnt) {
+	LOGINFO("Start building bounding volume hierarchie.")
+	return ::build(shapes, cnt, 0);
+	LOGINFO("Finish building bounding volume hierarchie.")
+}
+
+BVH::~BVH() {
+	// delete only BVH since the oder shapes are owned by the world
+	// TODO: better way organize this
+	delete dynamic_cast<BVH*>(this->left);
+	delete dynamic_cast<BVH*>(this->right);
+}
+
+bool BVH::hit(const Ray& ray, float tmin, float tmax, float time, HitRecord& rec) const {
+	if (!this->box.intersect(ray, tmin, tmax)) return false;
+
+	rec.t = tmax; // the left hit might upate that
+	// we have to call both branches since we can have 2 hits
+	bool ishit = this->left->hit(ray, tmin, tmax, time, rec);
+	bool ishit1 = this->right->hit(ray, tmin, rec.t, time, rec);
+
+	return ishit || ishit1;
+}
--- geometry/BVH.h
+++ geometry/BVH.h
@@ -5,7 +5,24 @@
 
 class BVH : public Shape {
 public:
+	BVH(Shape* l, Shape* r)
+		: left(l),
+		right(r),
+		box(BBox::bound(l->bbox(), r->bbox())) {}
+	BVH(Shape* l, Shape* r, const BBox& box)
+		: left(l),
+		right(r),
+		box(box) {}
 
+	static Shape* build(Shape** shapes, uint32_t cnt);
+
+	virtual ~BVH() override;
+	// not used, cannot be instanciated from the parser
+	virtual void parse(std::unordered_map<std::string, std::string>& map) override {}
+	virtual bool hit(const Ray& ray, float tmin, float tmax, float time, HitRecord& rec) const override;
+	virtual BBox bbox() const override { return this->box; }
 private:
-
+	Shape* left;
+	Shape* right;
+	BBox box;
 };
