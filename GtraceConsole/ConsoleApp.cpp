2b36fd9e4d366ddccc75d23313e899e50317a04b
Hristosko Chaushev <tosko_chauschev@abv.bg>
2021-05-06 21:30:46 +0300

    build and run console app

--- GtraceConsole/ConsoleApp.cpp
+++ GtraceConsole/ConsoleApp.cpp
@@ -1,3 +1,19 @@
+#include "renderer/RendererOutput.h"
+#include "renderer/Renderer.h"
+#include "scene/Parser.h"
+#include "scene/World.h"
+#include <string>
+
 int main(int argc, char** argv) {
+	const char* inputPath = argv[1];
+	const char* outputPath = argv[2];
+	RendererOutput output;
+	output.init();
+	SceneParser::parseFile(inputPath);
+	getWorld().buildBVH();
+	auto noopUpdater = []() {};
+	Renderer renderer(noopUpdater, output);
+	renderer.render();
+	output.save(outputPath);
 	return 0;
 }
