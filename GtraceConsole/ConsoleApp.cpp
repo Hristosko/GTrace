dfa141f31f70839f19d6fd6ce646697fe8a49679
Hristosko Chaushev <tosko_chauschev@abv.bg>
2021-05-06 21:43:54 +0300

    small improovments in the lib

--- GtraceConsole/ConsoleApp.cpp
+++ GtraceConsole/ConsoleApp.cpp
@@ -2,18 +2,29 @@
 #include "renderer/Renderer.h"
 #include "scene/Parser.h"
 #include "scene/World.h"
+#include "MemoryBench.h"
+#include "Logger.h"
 #include <string>
 
 int main(int argc, char** argv) {
-	const char* inputPath = argv[1];
-	const char* outputPath = argv[2];
-	RendererOutput output;
-	output.init();
-	SceneParser::parseFile(inputPath);
-	getWorld().buildBVH();
-	auto noopUpdater = []() {};
-	Renderer renderer(noopUpdater, output);
-	renderer.render();
-	output.save(outputPath);
+	MemoryBench::reset();
+	{
+		const char* inputPath = argv[1];
+		const char* outputPath = argv[2];
+		RendererOutput output;
+		output.init();
+		SceneParser::parseFile(inputPath);
+		getWorld().buildBVH();
+		auto noopUpdater = []() {};
+		Renderer renderer(noopUpdater, output);
+		renderer.render();
+		output.save(outputPath);
+		getWorld().clear();
+	}
+	const MemoryBench::Data mb = MemoryBench::get();
+	LOGSTAT("Total alocated memory: ", mb.totalAllocatedMemory, "B ", (float)mb.totalAllocatedMemory / (1024 * 1024), "MB");
+	LOGSTAT("Peak memory usage: ", mb.peakMemoryUsage, "B ", (float)mb.peakMemoryUsage / (1024 * 1024), "MB");
+	LOGSTAT("Alocations count: ", mb.allocationCount);
+	LOGSTAT("Freed allocations count: ", mb.freeCount);
 	return 0;
 }
