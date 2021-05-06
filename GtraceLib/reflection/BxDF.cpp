5a864f4cab78b92e8e1da59ea746be08de043a3c
Hristosko Chaushev <tosko_chauschev@abv.bg>
2021-05-06 21:54:19 +0300

    fix compilation and add some deleted files (error in the merge)

---
+++ GtraceLib/reflection/BxDF.cpp
@@ -0,0 +1,14 @@
+#include "BxDF.h"
+#include "../math/Utils.h"
+
+Vector3f BRDFtoBTDF::f(const Ray& ray, const HitRecord& hr, const Vector3f& wo, const Vector3f& wi) const {
+    return this->brdf->f(ray, hr, wo, otherHemisphere(wi));
+}
+
+Vector3f BRDFtoBTDF::sample(const Ray& ray, const HitRecord& hr, const Vector3f& wo, Vector3f& wi, float u1, float u2, float& pdf) const {
+    return this->brdf->sample(ray, hr, wo, wi, u1, u2, pdf);
+}
+
+Vector3f BRDFtoBTDF::rho(const Ray& ray, const HitRecord& hr, uint32_t nSamples, float* samples) const {
+    return this->brdf->rho(ray, hr, nSamples, samples);
+}
---
+++ GtraceLib/reflection/Fresnel.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../math/Vector3f.h"
+
+class Fresnel {
+public:
+	virtual ~Fresnel() {}
+
+	virtual Vector3f evaluate(float cosi) const = 0;
+protected:
+	static Vector3f circularyPolariezed(float coso, float cost, const Vector3f& etao, const Vector3f& etat);
+	static Vector3f conductorReflectance(float coso, const Vector3f& eta, const Vector3f& k);
+	static Vector3f approximateEta(const Vector3f& i);
+	static Vector3f approximateK(const Vector3f& i);
+};
